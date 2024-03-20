545dd7aa738cac190b77998601a186327599cd71
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-03-20 16:25:15 +0100

    feat: add game list on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,9 @@
+import GameList from '../components/GameList';
 import Header from '../components/Header';
+
+import rocketLeagueImage from '../assets/games/rocket-league.jpg';
+import csImage from '../assets/games/cs.jpg';
+import lolImage from '../assets/games/lol.jpg';
 
 function HomePage() {
     const title = 'Accueil';
@@ -9,9 +14,16 @@ function HomePage() {
         },
     ];
 
+    const games = [
+        { name: 'Rocket League', description: "C'est comme du foot, mais avec des voitures et du boost. Et c'est encore plus fun.", image: rocketLeagueImage, href: '/' },
+        { name: 'Counter-Strike', description: "C'est comme la guerre, mais avec des pistolets à clous et des bombes à C4.", image: csImage, href: '/' },
+        { name: 'League of Legends', description: "Quand tu perds un match de League of Legends, c'est toujours la faute de ton jungler.", image: lolImage, href: '/' },
+    ]
+
     return (
         <>
             <Header title={title} breadcrumbItems={breadcrumbItems} />
+            <GameList games={games} />
         </>
     );
 }
