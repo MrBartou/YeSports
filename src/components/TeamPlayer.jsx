a75d5dc5a19a41e35de58a0c50e0ee1b66859702
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-03-20 19:14:57 +0100

    feat: skeleton - other change

--- src/components/TeamPlayer.jsx
+++ src/components/TeamPlayer.jsx
@@ -11,35 +11,35 @@ function TeamPlayer({age, firstName, lastName, pseudo, image, nationality, role,
 
   return (
       <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" role="tablist">
+        <ul className="flex flex-wrap text-xs md:text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" role="tablist">
           <li className="mr-2">
-            <button onClick={() => handleTabChange('about')} type="button" className={`inline-block p-4 rounded-lg ${activeTab === 'about' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('about')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'about' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               À propos
             </button>
           </li>
           <li className="mr-2">
-            <button onClick={() => handleTabChange('autres')} type="button" className={`inline-block p-4 rounded-lg ${activeTab === 'autres' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('autres')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'autres' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               Autres
             </button>
           </li>
           <li className="mr-2">
-            <button onClick={() => handleTabChange('stats')} type="button" className={`inline-block p-4 rounded-lg ${activeTab === 'stats' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('stats')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'stats' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               Stats
             </button>
           </li>
         </ul>
-          <div className="p-4">
+          <div className="p-2 md:p-4">
               {activeTab === 'about' && (
-                <div className="bg-white rounded-lg md:p-8 dark:bg-gray-800">
-                    <img src={image} alt="Player Image" className="w-32 h-32 mx-auto rounded-full object-cover" />
-                    <h2 className="mt-3 text-center text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{pseudo}</h2>
-                    <p className="mt-2 text-center text-sm text-gray-500 dark:text-gray-400">{firstName} {lastName}</p>
-                    <p className="mt-2 text-center text-sm text-gray-400 dark:text-gray-300">{role}</p>
+                <div className="bg-white rounded-lg dark:bg-gray-800">
+                    <img src={image} alt="Player Image" className="w-24 md:w-32 h-24 md:h-32 mx-auto rounded-full object-cover" />
+                    <h2 className="mt-3 text-center text-xl md:text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white truncate">{pseudo}</h2>
+                    <p className="mt-2 text-center text-xs md:text-sm text-gray-500 dark:text-gray-400 truncate">{firstName} {lastName}</p>
+                    <p className="mt-2 text-center text-xs md:text-sm text-gray-400 dark:text-gray-300">{role}</p>
                 </div>
               )}
               {activeTab === 'autres' && (
                 <div className="bg-white rounded-lg md:p-8 dark:bg-gray-800">
-                    <ul className="space-y-4 text-gray-500 dark:text-gray-400">
+                    <ul className="space-y-2 md:space-y-4 text-gray-500 dark:text-gray-400">
                         <li>Age : {age}</li>
                         <li>Nationalité : {nationality}</li>
                         <li>Date de naissance : {birth}</li>
@@ -50,7 +50,7 @@ function TeamPlayer({age, firstName, lastName, pseudo, image, nationality, role,
               )}
               {activeTab === 'stats' && (
                 <div className="bg-white rounded-lg md:p-8 dark:bg-gray-800">
-                  <ul className="space-y-4 text-gray-500 dark:text-gray-400">
+                  <ul className="space-y-2 md:space-y-4 text-gray-500 dark:text-gray-400">
                     <li>Matches joués : {matchesPlayed}</li>
                     <li>Matches gagnés : {matchesWon}</li>
                     <li>Matches perdus : {matchesLost}</li>
--- src/pages/TeamInfo.jsx
+++ src/pages/TeamInfo.jsx
@@ -2,56 +2,68 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import TeamPlayer from '../components/TeamPlayer';
 import Sidebar from '../components/Sidebar';
+import SkeletonPlayer from '../components/SkeletonPlayer';
+import { fetchTeamData, fetchPlayerMatches } from '../service/ApiService'
 
 function TeamInfo() {
   let { teamSlug } = useParams();
   const [teamData, setTeamData] = useState(null);
   const [matchStats, setMatchStats] = useState([]);
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      authorization: 'Bearer AoQmVNGTseJUalgPE4kipVvn_5yPUJ7eZxdrI9K5K3jee2cZ3Bg'
-    }
-  };
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchTeamDataAndMatchStats = async () => {
       try {
-        const teamResponse = await fetch(`https://api.pandascore.co/teams/${teamSlug}`, options);
-        const team = await teamResponse.json();
+        setIsLoading(true);
+        const team = await fetchTeamData(teamSlug);
 
-        if (team && team.players && team.players.length > 0) {
+        if (team?.players?.length > 0) {
           setTeamData(team);
 
-          const statsPromises = team.players.map(player =>
-            fetch(`https://api.pandascore.co/players/${player.id}/matches`, options)
-              .then(response => response.json())
-          );
+          const statsPromises = team.players.map(player => fetchPlayerMatches(player.id));
           const matchesResults = await Promise.all(statsPromises);
 
           const newMatchStats = matchesResults.map((matches, index) => {
-            const player = team.players[index];
-            const stats = {
-              id: player.id,
+            const { id } = team.players[index];
+            return {
+              id,
               matchesPlayed: matches.length,
-              matchesWon: matches.filter(match => match.winner && match.winner.id === team.id).length,
+              matchesWon: matches.filter(match => match.winner?.id === team.id).length,
               matchesLost: matches.filter(match => match.winner && match.winner.id !== team.id).length
             };
-            return stats;
           });
 
           setMatchStats(newMatchStats);
         }
       } catch (error) {
         console.error("Erreur lors de la récupération des données:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchTeamDataAndMatchStats();
   }, [teamSlug]);
 
-  if (!teamData) return <div>Chargement...</div>;
+  if (isLoading) {
+    return (
+      <div className="min-h-screen dark:bg-gray-900">
+        <h1 className="text-3xl font-bold text-center text-gray-800 dark:text-white py-5">Chargement...</h1>
+        <div className="flex">
+          <div className="w-1/5">
+            <Sidebar />
+          </div>
+          <div className="flex-1 overflow-auto p-4">
+            <div className="grid grid-cols-3 gap-4">
+              <SkeletonPlayer />
+              <SkeletonPlayer />
+              <SkeletonPlayer />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen dark:bg-gray-900">
