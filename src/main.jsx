21000ab91d29b64321ad13f1490a09f2e7f09c97
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-03-20 11:53:11 +0100

    feat: add not found error page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NotFound from './pages/errors/NotFound.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <App />,
+    errorElement: <NotFound />
   },
 ]);
 
