1332dda25163ec93233efdffdbb36f9de72fcba6
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-03-20 16:25:03 +0100

    feat: add description on game card component

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function GameCard({ name, image, href }) {
+function GameCard({ name, description, image, href }) {
     return (
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <img className="rounded-t-lg" src={image} alt="" />
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">C'est comme du foot, mais avec des voitures et du boost. Et c'est encore plus fun.</p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
                 <Link to={href} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-emerald-700 rounded-lg hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">
                     Voir les équipes
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -21,6 +21,7 @@ function GameCard({ name, image, href }) {
 
 GameCard.propTypes = {
     name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     href: PropTypes.string.isRequired,
 };
