3ae3c177e9cec1dd4f202c8db89cf32084810c9f
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-03-20 14:12:57 +0100

    feat: add home page route

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import NotFound from './pages/errors/NotFound.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound />
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />
+      }
+    ]
   },
 ]);
 
