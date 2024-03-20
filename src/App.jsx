2b2cd2bcffef93d5cb96ffda89377fc74f2cc3c1
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-03-20 11:23:32 +0100

    feat: add router outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom"
 import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
     <div className="min-h-screen flex dark:bg-gray-900">
       <Sidebar />
+      <Outlet />
     </div>
   )
 }
