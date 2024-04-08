5cb3de3f0eba7ebf8a63e56e1d77976fda409f0a
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-04-08 16:10:41 +0200

    feat: add like to a player card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'flowbite';
 import PropTypes from 'prop-types';
 import avatarIcon from '../assets/avatar.png';
 
-function PlayerCard({age, firstName, lastName, pseudo, image, nationality, role, birth, equipe, matchesPlayed, matchesWon, matchesLost}) {
+function PlayerCard({id, age, firstName, lastName, pseudo, image, nationality, role, birth, equipe, matchesPlayed, matchesWon, matchesLost}) {
   const [activeTab, setActiveTab] = useState('about');
+  const [hasLiked, setHasLiked] = useState(false);
 
   const handleTabChange = (newTab) => {
     setActiveTab(newTab);
   };
 
+  useEffect(() => {
+    const liked = localStorage.getItem(`${id}-liked`) === 'true';
+    setHasLiked(liked);
+  }, [id]);
+
+  const toggleLike = () => {
+    setHasLiked(!hasLiked);
+    localStorage.setItem(`${id}-liked`, `${!hasLiked}`);
+  }
+
   return (
-      <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="relative w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <ul className="flex flex-wrap text-xs md:text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" role="tablist">
           <li className="mr-2">
             <button onClick={() => handleTabChange('about')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'about' ? 'text-emerald-600 dark:text-emerald-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
@@ -64,6 +75,19 @@ function PlayerCard({age, firstName, lastName, pseudo, image, nationality, role,
                   </ul>
                 </div>
               )}
+          </div>
+          <div className="absolute bottom-3 right-3">
+            <button className={`btn ${hasLiked ? 'btn-success' : 'btn-outline-primary'}`} onClick={toggleLike}>
+              {hasLiked ? (
+                <svg className="w-6 h-6 text-emerald-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"/>
+                </svg>              
+              ) : (
+                <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
+                </svg>
+              )}
+            </button>
           </div>
       </div>
   );
