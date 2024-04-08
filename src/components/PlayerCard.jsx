537399926cf100c2d53590a367bc6ff2cd9261c6
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-04-08 09:52:06 +0200

    fix: add default image on player card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import 'flowbite';
 import PropTypes from 'prop-types';
+import avatarIcon from '../assets/avatar.png';
 
 function PlayerCard({age, firstName, lastName, pseudo, image, nationality, role, birth, equipe, matchesPlayed, matchesWon, matchesLost}) {
   const [activeTab, setActiveTab] = useState('about');
@@ -13,17 +14,17 @@ function PlayerCard({age, firstName, lastName, pseudo, image, nationality, role,
       <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <ul className="flex flex-wrap text-xs md:text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" role="tablist">
           <li className="mr-2">
-            <button onClick={() => handleTabChange('about')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'about' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('about')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'about' ? 'text-emerald-600 dark:text-emerald-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               À propos
             </button>
           </li>
           <li className="mr-2">
-            <button onClick={() => handleTabChange('autres')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'autres' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('autres')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'autres' ? 'text-emerald-600 dark:text-emerald-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               Autres
             </button>
           </li>
           <li className="mr-2">
-            <button onClick={() => handleTabChange('stats')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'stats' ? 'text-blue-600 dark:text-blue-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
+            <button onClick={() => handleTabChange('stats')} type="button" className={`inline-block p-2 md:p-4 rounded-lg ${activeTab === 'stats' ? 'text-emerald-600 dark:text-emerald-500' : 'hover:text-gray-600 dark:hover:text-gray-300'} `} role="tab">
               Stats
             </button>
           </li>
@@ -31,7 +32,11 @@ function PlayerCard({age, firstName, lastName, pseudo, image, nationality, role,
           <div className="p-2 md:p-4">
               {activeTab === 'about' && (
                 <div className="bg-white rounded-lg dark:bg-gray-800">
-                    <img src={image} alt="Player Image" className="w-24 md:w-32 h-24 md:h-32 mx-auto rounded-full object-cover" />
+                    <img 
+                        src={image ? image : avatarIcon}
+                        alt="Player Image"
+                        className="w-24 md:w-32 h-24 md:h-32 mx-auto rounded-full object-cover"
+                    />
                     <h2 className="mt-3 text-center text-xl md:text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white truncate">{pseudo}</h2>
                     <p className="mt-2 text-center text-xs md:text-sm text-gray-500 dark:text-gray-400 truncate">{firstName} {lastName}</p>
                     {role && (
