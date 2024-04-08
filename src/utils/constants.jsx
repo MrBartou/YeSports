e176cd361e2df68b726e3e2160794ce277435165
DevLudo <roux.ludovic.pro@gmail.com>
2024-04-08 22:30:35 +0200

    feat: add some languages

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,7 @@
 export const LANGUAGES = [
     { label: 'Français', code: 'fr'},
     { label: 'English', code: 'en'},
+    { label: '日本語', code: 'jp' },
+    { label: 'Español', code: 'es' },
+    { label: 'Deutsch', code: 'de' },
 ]
