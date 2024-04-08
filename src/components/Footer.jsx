bab32eae028259cba36caed04e1554f350854a7a
DevLudo <60669196+DevLudo@users.noreply.github.com>
2024-04-08 11:23:41 +0200

    feat: remove shadow on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 mt-4">
+        <footer className="bg-white rounded-lg dark:bg-gray-900 mt-4">
             <div className="w-full max-w-screen-xl mx-auto pt-4">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
